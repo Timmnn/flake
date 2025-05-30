f2e2d4b641443a2643a76c32e8972a8b5d74886d
Timm Nicolaizik <timmmmnn@gmail.com>
2025-05-30 15:59:05 +0200

    Added VsCode Plugins

--- home/modules/vscode.nix
+++ home/modules/vscode.nix
@@ -8,20 +8,18 @@
       pkgs.vscode; # Or `pkgs.vscodium` if you prefer the open-source build
 
     extensions = with pkgs.vscode-extensions; [
-      ms-python.python
-      ms-vscode.cpptools
-      esbenp.prettier-vscode
-      vscodevim.vim
-      # Add more as needed
+      catppuccin.catppuccin-vsc
+      bbenoist.nix
+      brettm12345.nixfmt-vscode
     ];
 
     userSettings = {
       "editor.tabSize" = 2;
       "editor.formatOnSave" = true;
       "files.autoSave" = "onFocusChange";
-      "workbench.colorTheme" = "Default Dark+";
       "workbench.sideBar.location" = "right";
       "editor.fontSize" = 11;
+      "workbench.colorTheme" = "Catppuccin Macchiato";
     };
   };
 }
